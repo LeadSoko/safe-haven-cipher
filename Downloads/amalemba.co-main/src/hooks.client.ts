068903e61e11893c264e5d0bb37fa0813cc066c4
Raysons <39072786+Raysons@users.noreply.github.com>
2025-08-03 22:49:03 +0300

    Delete Downloads/amalemba.co-main directory

--- Downloads/amalemba.co-main/src/hooks.client.ts
+++
@@ -1,8 +0,0 @@
-// Remove unused imports and variables
-// import * as Sentry from '@sentry/sveltekit';
-// const dev = process.env.NODE_ENV !== 'production';
-
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-// handleErrorWithSentry((error) => {
-//   // Custom error handling logic
-// });
